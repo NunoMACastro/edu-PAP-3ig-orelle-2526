d28b8dd49e57795e2c451cd8900950f3d488b2d5
BrunaYasmi <brsantana23ig@epms.pt>
2026-05-29 19:32:14 +0000

    fix(backend): conecta com sucesso ao mongodb atlas

---
+++ orelle-project/apps/api/services/service.js
@@ -0,0 +1,13 @@
+const User = require('../models/user.model.js');
+const bcrypt = require('bcrypt');
+
+exports.registerUser = async (email, password) => {
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    const newUser = new User({
+        email,
+        passwordHash
+    });
+    return await newUser.save();
+};
