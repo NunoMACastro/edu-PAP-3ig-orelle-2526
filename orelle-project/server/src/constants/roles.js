3c04cf433be574c1c67ecc4b15ea3fc7f5178590
Nuno Castro <nunomacastro@gmail.com>
2026-06-01 19:08:59 +0100

    Update: Reset

--- orelle-project/server/src/constants/roles.js
+++
@@ -1,7 +0,0 @@
-export const ROLES = Object.freeze({
-    CLIENTE: "cliente",
-    CONSULTOR: "consultor",
-    ADMIN: "administrador",
-});
-
-export const ROLE_VALUES = Object.freeze(Object.values(ROLES));
